8f99561a92f8138398b2c302065dde2bac10f246
martincz <martincz@mokeedev.com>
2014-11-12 21:25:55 +0800

    mokee bringup

--- mokee.mk
+++ mokee.mk
@@ -16,13 +16,13 @@
 $(call inherit-product, device/sony/tianchi/full_tianchi.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
 
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit MK common Phone stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Build fingerprints
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D5303 BUILD_FINGERPRINT=Sony/D5303/D5303:4.4.3/19.1.1.A.0.165/Wv93Zw:user/release-keys PRIVATE_BUILD_DESC="D5303-user 4.4.3 19.1.1.A.0.165 Wv93Zw release-keys"
 
-PRODUCT_NAME := cm_tianchi
+PRODUCT_NAME := mk_tianchi
 PRODUCT_DEVICE := tianchi
